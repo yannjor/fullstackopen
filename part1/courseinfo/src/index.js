978f106cc9575ec3e9e1c01d2acf3af3df2a72cd
Yann <yann.jorelle@aalto.fi>
2020-03-24 16:48:09 +0200

    Add solution for 1b

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -15,7 +15,7 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <>
-      <p>{props.name} {props.exercises}</p>
+      <p>{props.parts.name} {props.parts.exercises}</p>
     </>
   );
 };
@@ -24,9 +24,7 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part name={props.name1} exercises={props.exercises1} />
-      <Part name={props.name2} exercises={props.exercises2} />
-      <Part name={props.name3} exercises={props.exercises3} />
+      {props.parts.map(part => <Part parts={part} />)}
     </>
   );
 };
@@ -34,27 +32,36 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.exercises.reduce((acc, val) => acc + val)}</p>
     </>
   );
 };
 
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content name1={part1} exercises1={exercises1} name2={part2}
-        exercises2={exercises2} name3={part3} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total exercises={course.parts.map(part => part.exercises)} />
     </div>
   );
 };
