dc8cc2c36dc6bcf5a07b0dccd1ecfb2154494359
Yann <yann.jorelle@aalto.fi>
2020-04-04 14:28:32 +0300

    Add solution for phonebook part 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+
+morgan.token('post_body', (req, res) => {
+    if (req.method === 'POST')
+        return JSON.stringify(req.body);
+});
+
 app.use(express.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post_body'));
 
 
 let persons = [
@@ -65,20 +73,6 @@ app.post('/api/persons', (req, res) => {
     persons = persons.concat(person);
     res.status(201).json(person);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 const PORT = 3001;
