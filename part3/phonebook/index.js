b50c0415923460b5d1a20c35fcb05f9827fff580
Yann <yann.jorelle@aalto.fi>
2020-04-06 16:09:23 +0300

    Fix malformatted id error

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -97,9 +97,9 @@ app.use(unknownEndpoint);
 
 const errorHandler = (error, request, response, next) => {
     console.log(error.message);
-    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' });
-    }
+    };
     next(error);
 };
 
