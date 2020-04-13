f59c24afe7fc2fd72edadf919b86df1b5eab8246
Yann <yann.jorelle@aalto.fi>
2020-04-13 21:23:16 +0300

    Add solution for exercise 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,23 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+const mongoUrl = process.env.MONGODB_URI;
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
