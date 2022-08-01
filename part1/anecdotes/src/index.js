441a47e7f1b92f716e14f7ad08a315a8632f4961
Yann Jorelle <yannjorelle@proton.me>
2022-08-01 15:59:32 +0300

    part1: update anecdotes exercise to 2022

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,45 +1,6 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const App = ({ anecdotes }) => {
-  const [selected, setSelected] = useState(0);
-  const [points, setPoints] = useState(Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf, 0));
+import App from "./App";
 
-  const handleVote = () => {
-    const newPoints = [...points];
-    newPoints[selected] += 1;
-    setPoints(newPoints);
-  };
-
-  const mostVotes = points.indexOf(Math.max(...points));
-
-  return (
-    <div>
-      <h1>Anecdote of the day</h1>
-      <p>{anecdotes[selected]}</p>
-      <p>Has {points[selected]} votes</p>
-      <button onClick={handleVote}>Vote</button>
-      <button onClick={() => setSelected(
-        Math.floor(Math.random() * (anecdotes.length))
-      )}>Next Anecdote</button>
-      <h1>Anecdote with most votes</h1>
-      <p>{anecdotes[mostVotes]}</p>
-      <p>Has {points[mostVotes]} votes</p>
-
-    </div >
-  );
-};
-
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-];
-
-ReactDOM.render(
-  <App anecdotes={anecdotes} />,
-  document.getElementById('root')
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
