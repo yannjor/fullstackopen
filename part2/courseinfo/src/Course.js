caf9343750f3c9f860f663112410d6753bd70273
Yann Jorelle <yannjorelle@proton.me>
2022-08-02 14:23:51 +0300

    part2: update courseinfo exercise to 2022

--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -1,40 +1,45 @@
-import React from 'react';
-
-
 const Header = ({ name }) => {
-    return <h2>{name}</h2>;
+  return <h2>{name}</h2>;
 };
 
-
-const Part = (props) => {
-    return <p>{props.parts.name} {props.parts.exercises}</p>;
+const Part = ({ parts }) => {
+  return (
+    <p>
+      {parts.name} {parts.exercises}
+    </p>
+  );
 };
-
 
 const Content = ({ parts }) => {
-    return (
-        <>
-            {parts.map(part => <Part key={part.id} parts={part} />)}
-        </>
-    );
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} parts={part} />
+      ))}
+    </>
+  );
 };
 
 const Total = ({ exercises }) => {
-    return (
-        <>
-            <p><strong>Total of {exercises.reduce((acc, val) => acc + val)} exercises</strong></p>
-        </>
-    );
+  return (
+    <>
+      <p>
+        <strong>
+          Total of {exercises.reduce((acc, val) => acc + val)} exercises
+        </strong>
+      </p>
+    </>
+  );
 };
 
 const Course = ({ course }) => {
-    return (
-        <>
-            <Header name={course.name} />
-            <Content parts={course.parts} />
-            <Total exercises={course.parts.map(part => part.exercises)} />
-        </>
-    );
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total exercises={course.parts.map((part) => part.exercises)} />
+    </>
+  );
 };
 
 export default Course;
