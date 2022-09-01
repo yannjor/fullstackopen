4b63842a28ef4c30201740321f6b339e803afe83
Yann Jorelle <yannjorelle@proton.me>
2022-09-01 14:59:48 +0300

    part9/calulator: 9.1-9.3

---
+++ part9/calculator/bmiCalculator.ts
@@ -0,0 +1,17 @@
+function calculateBmi(heightCm: number, massKg: number): string {
+  const bmi = massKg / Math.pow(heightCm / 100, 2);
+  if (bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi < 25) {
+    return "Normal (healthy weight)";
+  } else if (bmi < 30) {
+    return "Overweight";
+  } else {
+    return "Obese";
+  }
+}
+
+const height = Number(process.argv[2]);
+const mass = Number(process.argv[3]);
+
+console.log(calculateBmi(height, mass));
---
+++ part9/calculator/exerciseCalculator.ts
@@ -0,0 +1,49 @@
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const ratingDescription: Map<number, string> = new Map([
+  [1, "horrible"],
+  [2, "not too bad but could be better"],
+  [3, "good job"],
+]);
+
+function calculateRating(average: number, target: number): number {
+  const diff = average - target;
+  if (diff < -1) {
+    return 1;
+  } else if (diff <= 0) {
+    return 2;
+  } else {
+    return 3;
+  }
+}
+
+function calculateExercises(exerciseHours: number[], target: number): Result {
+  const periodLength = exerciseHours.length;
+  const average = exerciseHours.reduce((a, b) => a + b) / periodLength;
+  const rating = calculateRating(average, target);
+  return {
+    periodLength,
+    trainingDays: exerciseHours.filter((h) => h > 0).length,
+    success: average >= target,
+    rating,
+    ratingDescription: ratingDescription.get(rating) || "",
+    target,
+    average,
+  };
+}
+
+try {
+  const exerciseHours = process.argv.slice(3).map((h) => Number(h));
+  const target = Number(process.argv[2]);
+  console.log(calculateExercises(exerciseHours, target));
+} catch (exception) {
+  console.log("Failed to parse input arguments");
+}
